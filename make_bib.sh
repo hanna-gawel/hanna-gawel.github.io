b4704ac4c546e63cb015aa5efab30d0e7cdc7c22
Hanna Gaweł <gawehanna@gmail.com>
2023-03-14 11:40:30 +0100

    Delete make_bib.sh

--- make_bib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Downloadign google scholar"
-wget -O scholar.bib $1
-echo "Covnerting"
-pandoc scholar.bib --citeproc --csl nature.csl -t plain --wrap=none -o biblio.txt && rm scholar.bib
